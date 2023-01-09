7e14d3a5a502c292f658b37f3214fd4d85b62e50
G-NA <heo_1325@naver.com>
2023-01-09 14:42:16 +0900

    update

---
+++ script.js
@@ -0,0 +1,36 @@
+const msgBox = document.querySelector("#inform-msg");
+const container = document.querySelector("#d-day-counter");
+container.style.display = "none";
+msgBox.textContent = "d-day를 입력해주세요.";
+
+const dateFormMaker = function () {
+  const year = document.querySelector("#date-input-year").value;
+  const month = document.querySelector("#date-input-month").value;
+  const day = document.querySelector("#date-input-day").value;
+
+  //const dateFormat = year + '-' + month + '-' + day;
+  const dateFormat = `${year}-${month}-${day}`; // 템플릿 리터럴
+  return dateFormat;
+};
+
+const counterMaker = function () {
+  const inputDate = dateFormMaker();
+  const targetDate = new Date(inputDate).setHours(0, 0, 0, 0); //setHours(0,0,0,0) 자정을 기준으로 시각 설정
+  const nowDate = new Date();
+  const temp = (targetDate - nowDate) / 1000; // return된 초 단위 결과값
+
+  if (temp <= 0) {
+    return console.log("타이머가 종료되었습니다.");
+  } else if (isNaN(temp)) {
+    // Date() 생성자 함수는 날짜 값만 반환. 날짜가 아닌 경우 NaN반환
+    //NaN값 확인을 위해서는 isNaN() 함수 사용, 비교 연산자 이용 불가
+    return console.log("유효한 시간대가 아닙니다.");
+  }
+
+  const days = Math.floor(temp / 3600 / 24); //Math.floor 소수점 이하 버림
+  const hours = Math.floor(temp / 3600) % 24;
+  const minutes = Math.floor(temp / 60) % 60;
+  const seconds = Math.floor(temp) % 60;
+
+  console.log(days, hours, minutes, seconds);
+};
