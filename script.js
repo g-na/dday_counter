9931fbb3eb0edbbb288b88447f674ba489c6ac51
G-NA <heo_1325@naver.com>
2023-01-11 14:00:45 +0900

    interval()
    interval 고유 id 값과 clearInterval() 이용하여 타이머 초기화 기능 구현

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector("#d-day-counter");
 container.style.display = "none";
 //msgBox.textContent = "d-day를 입력해주세요.";
 msgBox.innerHTML = "<h3>D-day를 입력해주세요.";
+const intervalIdArr = [];
 
 const dateFormMaker = function () {
   const year = document.querySelector("#date-input-year").value;
@@ -70,7 +71,19 @@ const counterMaker = function () {
 const starter = function () {
   container.style.display = "flex";
   msgBox.style.display = "none";
-
   counterMaker(); // setInterval 최초 실행시 1초 간격 사이 0시간 0분 0초 나오는 현상 해결
-  setInterval(counterMaker, 1000); // setInterval(x,y): x함수를 y초마다 계속해서 반복
+  // setInterval(x,y): x함수를 y초마다 계속해서 반복
+  // 실행할 때마다 interval에 고유 id값이 반영됨
+  const intervalId = setInterval(counterMaker, 1000);
+  intervalIdArr.push(intervalId);
+};
+
+const setClearInterval = function () {
+  //counterMaker 함수에서  사용중인 데이터 초기화
+  container.style.display = "none";
+  msgBox.innerHTML = "<h3>D-Day를 입력해주세요.</h3>";
+  msgBox.style.display = "flex";
+  for (let j = 0; j < intervalIdArr.length; j++) {
+    clearInterval(intervalIdArr[j]);
+  }
 };
